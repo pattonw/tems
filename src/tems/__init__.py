e88480e823b8d789cff36c15a5183f4eada259db
William Patton <wllmpttn24@gmail.com>
2025-05-24 08:59:40 -0700

    bump version 1.0.3

--- src/tems/__init__.py
+++ src/tems/__init__.py
@@ -4,5 +4,5 @@ from .scale import Upsample as Upsample
 from .umodule import UModule as UModule
 from .unet import UNet as UNet
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
